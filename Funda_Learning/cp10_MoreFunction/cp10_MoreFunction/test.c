3b60b5f20bd228f0e1a759b7396cbb971d427241
pharmature <pharmature23@gmail.com>
2025-02-28 14:35:40 +0800

    字符串函数

--- Funda_Learning/cp10_MoreFunction/cp10_MoreFunction/test.c
+++ Funda_Learning/cp10_MoreFunction/cp10_MoreFunction/test.c
@@ -111,45 +111,121 @@
 
 // 模拟实现
 
-char* my_strcat(char* dest, const char* src)
+//char* my_strcat(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest && src);
+//	while (*dest != '\0')
+//	{
+//		dest++;
+//	}
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[20] = "hello "; // 目标空间必须足够大
+//	char arr2[] = "world";
+//
+//	//strcat(arr1, arr2);
+//	/*my_strcat(arr1, arr2);
+//	printf("%s\n", arr1);*/
+//
+//	//printf("%s\n", my_strcat(arr1, arr2));
+//
+//	// 字符串自己给自己追加 -- 死循环
+//	printf("%s\n", my_strcat(arr1, arr1));
+//	
+//	return 0;
+//}
+
+
+// 4. strcmp() - 字符串比较
+// int strcmp(const char* str1, const char* str2)
+// str1 大于 str2，返回大于0的数字
+// str1 等于 str2，返回0
+// str1 小于 str2，返回小于0的数字
+
+// 模拟实现
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//			return 0;
+//		str1++;
+//		str2++;
+//	}
+//	return *str1 - *str2;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abb";
+//	
+//	//int ret = strcmp(arr1, arr2);
+//	int ret = my_strcmp(arr1, arr2);
+//
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+// 以上都是长度不受限制的字符串函数，还有几个限制长度的函数
+// strncpy(char* dest, const char* src, size_t num)
+// 拷贝 num 个字符从源字符串到目标空间
+// 如果源字符串的长度小于num，则拷贝完之后，在目标的后面追加0，直到num个
+
+// strncat(char* dest, const char* src, size_t num)
+// 追加 num 个字符从源字符串到目标空间
+// 如果源字符串的长度小于num，不会追加0
+
+// strncmp(const char* str1, const char* str2, size_t num)
+// 比较到出现另个字符不一样，或者一个字符串结束，或者num个字符全部比较完
+
+
+// 5. strstr() - 寻找字符串
+// 在第一个字符串中寻找第二个字符串第一次出现的位置，没有则返回NULL
+
+// 模拟实现 - 暴力查找算法
+char* my_strstr(const char* str1, const char* str2)
 {
-	char* ret = dest;
-	assert(dest && src);
-	while (*dest != '\0')
+	char* s1 = NULL;
+	char* s2 = NULL;
+
+	while (str1)
 	{
-		dest++;
+		s1 = (char*)str1;
+		s2 = (char*)str2;
+		while (*s1 && *s2 && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+			return (char*)str1;
+		str1++;
 	}
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	return ret;
+	return NULL;
 }
 
 int main()
 {
-	char arr1[20] = "hello "; // 目标空间必须足够大
-	char arr2[] = "world";
+	char arr1[] = "abbbbcdef";
+	char arr2[] = "bcd";
 
-	//strcat(arr1, arr2);
-	/*my_strcat(arr1, arr2);
-	printf("%s\n", arr1);*/
+	//char* p = strstr(arr1, arr2);
+	char* p = my_strstr(arr1, arr2);
 
-	//printf("%s\n", my_strcat(arr1, arr2));
-
-	// 字符串自己给自己追加
-	printf("%s\n", my_strcat(arr1, arr1));
-	
+	printf("%s\n", p);
 	return 0;
 }
-
-
-
-
-
-
-
-
 
 
 
