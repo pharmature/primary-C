0a1e8ff2c6db442f715604d7b1e1de7983f9b5b9
pharmature <pharmature23@gmail.com>
2025-03-01 16:50:29 +0800

    内存函数

--- Funda_Learning/cp10_MoreFunction/cp10_MoreFunction/test.c
+++ Funda_Learning/cp10_MoreFunction/cp10_MoreFunction/test.c
@@ -178,6 +178,7 @@
 
 
 // 以上都是长度不受限制的字符串函数，还有几个限制长度的函数
+
 // strncpy(char* dest, const char* src, size_t num)
 // 拷贝 num 个字符从源字符串到目标空间
 // 如果源字符串的长度小于num，则拷贝完之后，在目标的后面追加0，直到num个
@@ -194,42 +195,297 @@
 // 在第一个字符串中寻找第二个字符串第一次出现的位置，没有则返回NULL
 
 // 模拟实现 - 暴力查找算法
-char* my_strstr(const char* str1, const char* str2)
-{
-	char* s1 = NULL;
-	char* s2 = NULL;
+//char* my_strstr(const char* str1, const char* str2)
+//{
+//	char* s1 = NULL;
+//	char* s2 = NULL;
+//
+//	while (str1)
+//	{
+//		s1 = (char*)str1;
+//		s2 = (char*)str2;
+//		while (*s1 && *s2 && *s1 == *s2)
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')
+//			return (char*)str1;
+//		str1++;
+//	}
+//	return NULL;
+//}
+//// 还有更优算法 - KMP
+//
+//int main()
+//{
+//	char arr1[] = "abbbbcdef";
+//	char arr2[] = "bcd";
+//
+//	//char* p = strstr(arr1, arr2);
+//	char* p = my_strstr(arr1, arr2);
+//
+//	printf("%s\n", p);
+//	return 0;
+//}
 
-	while (str1)
-	{
-		s1 = (char*)str1;
-		s2 = (char*)str2;
-		while (*s1 && *s2 && *s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		if (*s2 == '\0')
-			return (char*)str1;
-		str1++;
-	}
-	return NULL;
-}
 
+// 6. strtok() - 分割字符串
+// char* strtok(char* str, const char* sep)
+// sep参数是一个字符串，定义了用作分隔符的字符合集
+// 第一个参数指定一个字符串，它包含了0个或者多个由sep字符串中一个或者多个分隔符分割的标记
+// strtok()函数找到str中的下一个标记，并将其用'\0'结尾，返回一个指向这个标记的指针
+// 注：strtok()函数会改变被操作的字符串，所以使用strtok()函数切的分字符串一般都是临时拷贝的内容并且可修改
+// strtok()函数的第一个参数不为NULL，函数找到str中第一个标记，strtok函数将保存它在字符串中的位置
+// strtok()函数的第一个参数为NULL，函数将在同一个字符串中被保存的位置开始，查找下一个标记
+// 如果字符串中不存在更多的标记，则返回 NULL 指针
+
+//int main()
+//{
+//	//char arr[] = "pharmature@foxmail.com";
+//	char arr[] = "192.163.255.255";
+//	char cup[30] = { 0 };
+//	strcpy(cup, arr); // 临时拷贝
+//
+//	//char* sep = "@.";
+//	char* sep = ".";
+//
+//	char* str = NULL;
+//	for (str = strtok(cup, sep); str != NULL; str = strtok(NULL, sep))
+//	{
+//		printf("%s\n", str);
+//	}
+//
+//	return 0;
+//}
+
+
+// 7. strerror() - 获取错误信息
+// char* strerror(int errnum)
+// 返回错误码所对应的错误信息
+
+//int main()
+//{
+//	char* p = strerror(0);
+//	printf("%s\n", p);
+//
+//	p = strerror(1);
+//	printf("%s\n", p);
+//
+//	p = strerror(2);
+//	printf("%s\n", p);
+//
+//	p = strerror(3);
+//	printf("%s\n", p);
+//	return 0;
+//}
+
+// C语言的库函数在调用失败的时候，会将错误码存放在一个叫 errno 的变量中
+// 当我们想知道调用库函数的时候发生了什么错误，就可以将errno的错误码翻译成错误信息
+//#include <errno.h>
+//int main()
+//{
+//	// 打开文件
+//	FILE* p = fopen("test.txt", "r");
+//	if (p == NULL)
+//	{
+//		printf("文件打开失败，错误信息：%s\n", strerror(errno));
+//		return 1;
+//	}
+//
+//	// 读写文件
+//
+//	// 关闭文件
+//	fclose(p);
+//	p = NULL;
+//	return 0;
+//}
+
+
+// perror() - 打印错误信息
+//#include <errno.h>
+//int main()
+//{
+//	// 打开文件
+//	FILE* p = fopen("test.txt", "r");
+//	if (p == NULL)
+//	{
+//		perror("文件打开失败"); - 自带冒号
+//		return 1;
+//	}
+//
+//	// 读写文件
+//
+//	// 关闭文件
+//	fclose(p);
+//	p = NULL;
+//	return 0;
+//}
+
+
+// 8. memcpy() - 内存拷贝
+// void* memcpy(void* dest, const void* src, size_t num);
+// 从src的位置向后复制num个字节的数据到dest的内存位置
+// 这个函数在遇到'\0'的时候并不会停下来
+// 使用 void* 的原因是作者不知道具体拷贝的是什么类型的数据
+
+// 如果src和dest有任何的重叠，复制的结果是未定义的
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[8] = { 0 };
+//	//把arr1中的前5个数据拷贝到arr2中
+//	memcpy(arr2, arr1, 20);
+//	return 0;
+//}
+
+// 模拟实现
+//#include <assert.h>
+//void* my_memcpy(void* dest, const void* src, size_t num)
+//{
+//	assert(dest && src);
+//	void* ret = dest;
+//	while (num--)
+//	{
+//		*(char*)dest = *(char*)src;
+//		dest = (char*)dest + 1;
+//		src = (char*)src + 1;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[8] = { 0 };
+//	//my_memcpy(arr2, arr1, 20);
+//
+//	// 发生重叠的情况
+//	//my_memcpy(arr1 + 2, arr1, 20); //error
+//	memcpy(arr1 + 2, arr1, 20); // VS环境下可以使用，但是其它环境下结果未知
+//
+//	return 0;
+//}
+// C语言规定：memcpy()拷贝不重叠的内存
+// VS环境下可以使用
+
+
+// 9. memmove() - 重叠的内存拷贝
+// void* memmove(void* dest, const void* src, size_t num);
+// 和memcpy的差别就是memmove函数处理的源内存块和目标内存块是可以重叠的
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[8] = { 0 };
+//	memmove(arr1 + 2, arr1, 20);
+//
+//	return 0;
+//}
+
+// 模拟实现
+
+//void* my_memmove(void* dest, const void* src, size_t num)
+//{
+//	assert(dest && src);
+//	void* ret = dest;
+//
+//	// 从前 -> 后
+//	if (dest < src)
+//	{
+//		while (num--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			dest = (char*)dest + 1;
+//			src = (char*)src + 1;
+//		}
+//	}
+//	// 从后 -> 前
+//	else
+//	{
+//		while (num--)
+//		{
+//			*((char*)dest + num) = *((char*)src + num);
+//		}
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	my_memmove(arr1 + 2, arr1, 20);
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//
+//	return 0;
+//}
+
+// 10. memcmp() - 内存比较
+//void* memcmp(const void* ptr1, const void* ptr2, size_t num);
+//比较从ptr1和ptr2指针开始的num个字节
+//返回值和strcmp()、strncmp()函数相同
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 1,2,3,4,6 };
+//	printf("%d\n", memcmp(arr1, arr2, 17));
+//	return 0;
+//}
+
+// 11. memset() - 内存设置
+//void* memset(void* ptr, int value, size_t num)
+//从ptr开始，将num个字节的内存设置为value
+
+//int main()
+//{
+//	char arr[] = "hello world";
+//	memset(arr, 'x', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+// 字符分类函数
+//iscntrl - 任何控制字符
+//isspace - 空白字符：空格' ',换页'\f'，换行'\n',回车'\r'，制表符'\t'，垂直制表符'\v'
+//isdigit - 十进制数字0~9
+//isxdigit - 十六进制数字，包括十进制数字，小写字母a~f,大写字母A~F
+//islower - 小写字母a~z
+//isupper - 大写字母A~Z
+//isalpha - 字母a~z或则A~Z
+//isalnum - 字母或者数字，a~z,A~Z,0~9,
+//ispunct - 标点符号，任何不属于数字或者字母的图形字符(可打印)
+//isgraph - 任何图形字符
+//isprint - 任何可打印字符，包括图形字符和空白字符
+
+
+// 字符转换
+//tolower - 转换成小写
+//toupper - 转换成大写
+
+#include <ctype.h>
 int main()
 {
-	char arr1[] = "abbbbcdef";
-	char arr2[] = "bcd";
+	char arr[30] = { 0 };
+	gets(arr);
 
-	//char* p = strstr(arr1, arr2);
-	char* p = my_strstr(arr1, arr2);
-
-	printf("%s\n", p);
+	int i = 0;
+	while (arr[i])
+	{
+		if (isupper(arr[i]))
+		{
+			arr[i] = tolower(arr[i]);
+		}
+		printf("%c", arr[i]);
+		i++;
+	}
 	return 0;
 }
-
-
-
-
-
 
 
