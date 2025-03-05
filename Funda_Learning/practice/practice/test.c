e4eb12e33cf5da636567d628bced862ed1482fdb
pharmature <pharmature23@gmail.com>
2025-03-05 12:17:08 +0800

    练习

--- Funda_Learning/practice/practice/test.c
+++ Funda_Learning/practice/practice/test.c
@@ -943,7 +943,399 @@
 //}
 
 
+//5位运动员参加了10米台跳水比赛，有人让他们预测比赛结果：
+//A选手说：B第二，我第三；
+//B选手说：我第二，E第四；
+//C选手说：我第一，D第二；
+//D选手说：C最后，我第三；
+//E选手说：我第四，A第一；
+//比赛结束后，每位选手都说对了一半，请编程确定比赛的名次。
 
+//int main()
+//{
+//	int a = 0, b = 0, c = 0, d = 0, e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (a * b * c * d * e == 120)
+//						{
+//							if ((b == 2) + (a == 3) == 1
+//								&& (b == 2) + (e == 4) == 1
+//								&& (c == 1) + (d == 2) == 1
+//								&& (c == 5) + (d == 3) == 1
+//								&& (e == 4) + (a == 1) == 1)
+//							{
+//								printf("%d %d %d %d %d\n", a, b, c, d, e);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+
+//日本某地发生了一件谋杀案，警察通过排查确定杀人凶手必为4个嫌疑犯的一个。以下为4个嫌疑犯的供词:
+//A说：不是我。
+//B说：是C。
+//C说：是D。
+//D说：C在胡说
+//已知3个人说了真话，1个人说的是假话。
+//现在请根据这些信息，写一个程序来确定到底谁是凶手。
+
+//int main()
+//{
+//	char killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("%c\n", killer);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+// 在屏幕上打印杨辉三角。
+
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			// 第一列全为1,正对角线全为1
+//			if (j == 0 || j == i)
+//				arr[i][j] = 1;
+//
+//			//填充其余数字
+//			if (i > 1 && j > 0 && j < i)
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//		}
+//	}
+//
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%-3d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的，请编写程序在这样的矩阵中查找某个数字是否存在。
+//要求：时间复杂度小于O(N);
+
+//int is_exist(int arr[3][3], int row, int col, int k)
+//{
+//	int x = 0;
+//	int y = col - 1;
+//	
+//	while (x <= row - 1 && y >= 0)
+//	{
+//		if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else if (arr[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else
+//		{
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 0;
+//	scanf("%d", &k);
+//	int ret = is_exist(arr,3,3, k);
+//	if (1 == ret)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//	return 0;
+//}
+
+
+//实现一个函数，可以左旋字符串中的k个字符。
+//例如：
+//ABCD左旋一个字符得到BCDA
+//ABCD左旋两个字符得到CDAB
+
+//#include <assert.h>
+//void reverse(char* left, char* right)
+//{
+//	assert(left && right);
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//void left_move(char* str, int k)
+//{
+//	//暴力移动
+//	//int len = strlen(str);
+//	//k %= len;
+//	//while (k > 0)
+//	//{
+//	//	//每次移动一个字符
+//	//	char tmp = str[0];
+//	//	//转移其余字符
+//	//	for (int j = 1; j < len; j++)
+//	//	{
+//	//		str[j - 1] = str[j];
+//	//	}
+//	//	//填充最后一个
+//	//	str[len - 1] = tmp;
+//	//	
+//	//	k--;
+//	//}
+//
+//	//三次倒置
+//	int len = strlen(str);
+//	k %= len;
+//	reverse(str, str + k - 1);
+//	reverse(str + k, str + len-1);
+//	reverse(str, str + len - 1);
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//
+//	printf("%s\n", arr);
+//	left_move(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//写一个函数，判断一个字符串是否为另外一个字符串旋转之后的字符串。
+//例如：给定s1 = AABCD和s2 = BCDAA，返回1
+//给定s1 = abcd和s2 = ACBD，返回0.
+//AABCD左旋一个字符得到ABCDA
+//AABCD左旋两个字符得到BCDAA
+//AABCD右旋一个字符得到DAABC
+
+//int is_left_move(char* s1, char* s2)
+//{
+//	//暴力查找
+//	/*int len = strlen(s1);
+//	for (int i = 0; i < len - 1; i++)
+//	{
+//		char tmp = s1[0];
+//		for (int j = 1; j < len; j++)
+//		{
+//			s1[j - 1] = s1[j];
+//		}
+//		s1[len - 1] = tmp;
+//
+//		if (0 == strcmp(s1, s2))
+//			return 1;
+//	}
+//	return 0;*/
+//
+//	//拼接后查找
+//	int len1 = strlen(s1);
+//	int len2 = strlen(s2);
+//	if (len1 != len2)
+//		return 0;
+//
+//	strncat(s1, s1, len1);
+//	if (strstr(s1, s2))
+//		return 1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char s1[20] = "ABCDEF";
+//	char s2[] = "CDEFA";
+//	int ret = is_left_move(s1, s2);
+//
+//	if (1 == ret)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int aa[2][5] = { 10,9,8,7,6,5,4,3,2,1 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
+
+
+//练习使用库函数，qsort排序各种类型的数据
+//void qsort(void* base,  // 指向待排序数组的第一个元素
+//			size_t num,  // 待排序的元素个数
+//			size_t size,  // 每个元素的大小，单位是字节
+//			int(*cmp)(const void* p1, const void* p2)); //指向一个比较函数，可以比较两个元素的大小
+
+//排序整型数组
+//#include <stdlib.h>
+//int int_cmp(const void* p1, const void* p2)
+//{
+//	return (*(int*)p1) - (*(int*)p2);
+//}
+//int main()
+//{
+//	int arr[] = { 7,3,6,2,8,1,9,4,5,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), int_cmp);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//排序字符数组
+//#include <stdlib.h>
+//int char_tmp(const void* p1, const void* p2)
+//{
+//	return (*(char*)p1) - (*(char*)p2);
+//}
+//int main()
+//{
+//	char str[] = "kjhysgb";
+//	int len = strlen(str);
+//	qsort(str, len, sizeof(str[0]), char_tmp);
+//	printf("%s\n", str);
+//	return 0;
+//}
+
+
+//排序结构体
+//#include <stdlib.h>
+//
+//typedef struct Stu
+//{
+//	char name[20];
+//	int age;
+//}stu;
+//
+//int stru_cmp(const void* p1, const void* p2)
+//{
+//	return strcmp(((stu*)p1)->name, ((stu*)p2)->name);
+//}
+//
+//int main()
+//{
+//	stu arr[] = { {"zhangsan",15},{"lisi",14}, {"wangwu",16} };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//按姓名排序
+//	qsort(arr, sz, sizeof(arr[0]), stru_cmp);
+//
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%s %d\n", (arr + i)->name, (arr + i)->age);
+//	}
+//	return 0;
+//}
+
+
+//模仿qsort的功能实现一个通用的冒泡排序
+typedef struct Stu
+{
+	char name[20];
+	int age;
+}stu;
+
+int stru_cmp(const void* p1, const void* p2)
+{
+	return strcmp(((stu*)p1)->name, ((stu*)p2)->name);
+}
+
+void swap(char* ps1, char* ps2, size_t size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		char tmp = *ps1;
+		*ps1 = *ps2;
+		*ps2 = tmp;
+		ps1++;
+		ps2++;
+	}
+}
+
+void bubble_qsort(void* base, size_t num, size_t size, int (*cmp)(const void* p1, const void* p2))
+{
+	for (int i = 0; i < num-1; i++)
+	{
+		int flag = 1; //假设排序已经完成
+		for (int j = 0; j < num - i - 1; j++)
+		{
+			if (cmp((char*)base + j * size, (char*)base + (j + 1) * size) > 0)
+			{
+				flag = 0;
+				swap((char*)base + j * size, (char*)base + (j + 1) * size, size);
+			}
+		}
+		if (1 == flag)
+			break;
+	}
+}
+
+int main()
+{
+	stu arr[] = { {"zhangsan",15},{"lisi",14}, {"wangwu",16} };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//按姓名排序
+	bubble_qsort(arr, sz, sizeof(arr[0]), stru_cmp);
+
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%s %d\n", (arr + i)->name, (arr + i)->age);
+	}
+	return 0;
+}
 
 
 
